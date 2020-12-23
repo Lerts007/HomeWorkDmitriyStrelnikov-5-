450b2520ed9e9160e997956b03e55fd8da866e01
Lerts <oddff@mail.ru>
2020-12-23 23:17:30 +0300

    Задание 3

---
+++ HomeWorkDmitriyStrelnikov-5-/Task_3/Message.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace Lesson_5_Task_3
+{
+    public class Message
+    {
+        string s1;
+        string s2;
+        public Message()
+        {
+            Console.Write("Введите первую строку: ");
+            string s1 = Console.ReadLine();
+            this.s1 = s1;
+
+            Console.Write("Введите  вторую  строку: ");
+            string s2 = Console.ReadLine();
+            this.s2 = s2;
+
+            Permutation();
+        }
+
+        public void Permutation()
+        {
+            if (s1.Length == s2.Length)
+            {
+                bool a = true;
+                for (int i = 0; i < s1.Length; i++)
+                {
+                    if (a)
+                    {
+                        for (int j = 0; j < s2.Length; j++)
+                        {
+                            if (s1[i] == s2[j])
+                            {
+                                a = true;
+                                break;
+                            }
+                            else
+                            {
+                                a = false;
+                            }
+                        }
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+                if (a)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nСтроки являются перестановкой.\n");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("\nВ строках различные символы. Строки не являются перестановкой.\n");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("\nСтроки разной длины не являются перестановкой.\n");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ HomeWorkDmitriyStrelnikov-5-/Task_3/Option.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Lesson_5_Task_3
+{
+    public class Option
+    {
+        public Option()
+        {
+            bool a = true;
+            while (a)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("Чтобы начать выполнение программы нажмите - 1" +
+                    "\nЧтобы закончить выполнение прогрммы нажмите - 2");
+                Console.ResetColor();
+                int b = int.Parse(Console.ReadLine());
+
+                switch (b)
+                {
+                    case 1:
+
+                        var c = new Message();
+
+                        break;
+
+                    case 2:
+
+                        a = false;
+
+                        break;
+                }
+            }
+        }
+    }
+}
